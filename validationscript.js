bc434adc359f2762abb3c9cab3e9bd21c867218e
VICOrepo <viral.jain.2019@ecajmer.ac.in>
2023-03-21 21:49:43 +0530

    updatedfile5

--- validationscript.js
+++ validationscript.js
@@ -1,53 +1,18 @@
-// function validateForm(){
-
-//     let loanPurpose = document.getElementById("loanPurpose").value
-
-//      let purpose = /^[a-zA-Z\s]+$/;
-
-//      if(!purpose.test(loanPurpose)){
-//         document.getElementById("loanPurpose").style.border = "3px solid red"
-//         document.getElementById("loanPurposError").innerHTML = "<span style='color: red;'>" + "Write a valid Reason";
-//         return false;
-//      }
-
-//      function dollar() {
-//       let dollarUS = Intl.NumberFormat("en-US", {
-//         style: "currency",
-//         currency: "USD",
-//       });
-
-//       let loanAmount1 = document.getElementById("loanAmount").value;
-//       let loanAmount2 = loanAmount1.replace("$", "");
-//       let loanAmount3 = loanAmount2.replace(",", "");
-//       document.getElementById("loanAmount").value = dollarUS.format(loanAmount3);
-
-//       let rate = loanAmount3 < 2000 || loanAmount3 == "" || loanAmount3 > 20000;
-//       document.getElementById("loanAmountError").style.display = rate
-//         ? "inline"
-//         : "none";
-//     }
-//      function Ispurpose() {
-//       var regPass = /^[a-zA-Z\s]+$/;
-//       loanPurpose = document.getElementById("loanPurpose").value;
-//       var rate = regPass.test(loanPurpose);
-//       document.getElementById("loanPurposeError").style.display = rate
-//         ? "none"
-//         : "inline";
-//       return rate;
-//     }
-// }
 
 function validateForm() {
   let loanAmount = document.getElementById("loanAmount");
   let loanPurpose = document.getElementById("loanPurpose");
   let firstName = document.getElementById("firstName");
+
   let lastName = document.getElementById("lastName");
   let email = document.getElementById("email");
-
-  let regname = "/^[a-zA-Z]+$/";
-  let regAmt =
-    "/^\$?(?!0.00)(([0-9]{1,3},([0-9]{3},)*)[0-9]{3}|[0-9]{1,3})(\.[0-9]{2})?$/";
-//  let regPur = "^\\s+[A-Za-z,;'\"\\s]+[.?!]$";
+  let zip = document.getElementById("zip")
+   regPhone = regMPhone = /^\d{10}$/
+   regname = /^[a-zA-Z]+$/;
+   regZip = /^\d{6}$/
+  regAmt =
+    /^\$?(?!0.00)(([0-9]{1,3},([0-9]{3},)*)[0-9]{3}|[0-9]{1,3})(\.[0-9]{2})?$/
+    let regPur = "^\\s+[A-Za-z,;'\"\\s]+[.?!]$";
 
   if (loanAmount.value.trim() == "" || !regAmt.test(loanAmount.value)) {
     loanAmount.style.border = "solid 3px red";
@@ -72,6 +37,7 @@ function validateForm() {
     return true;
   }
 }
+
 function emailValidation(){
    let email = document.getElementById("email")
    let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
@@ -276,7 +242,12 @@ else {
 // }
 function firstNameValidation(){
    let firstName = document.getElementById("firstName")
+   let name = /^[a-zA-Z ]*$/;
    if(firstName.value.trim() == ""){
+      document.getElementById("firstnameError").style.visibility="visible";
+      return false;
+   }
+   else if(!firstName.value.match(name)){
       document.getElementById("firstnameError").style.visibility="visible";
       return false;
    }
@@ -287,8 +258,13 @@ function firstNameValidation(){
 }
 
 function lastNameValidation(){
-   let firstName = document.getElementById("lastName")
-   if(firstName.value.trim() == ""){
+   let lastName = document.getElementById("lastName")
+   let lastn = /^[a-zA-Z ]*$/;
+   if(lastName.value.trim() == ""){
+      document.getElementById("lastnameError").style.visibility="visible";
+      return false;
+   }
+   else if(!lastName.value.match(lastn)){
       document.getElementById("lastnameError").style.visibility="visible";
       return false;
    }
@@ -299,29 +275,7 @@ function lastNameValidation(){
 }
 
 
-var specialKeys = new Array();
-specialKeys.push(8); 
-function isAlphaFirstName(e){
-     document.getElementById("firstName").innerHTML = "";
-     var keyCode = e.which ? e.which : e.keyCode;
-     var ret =
-       (keyCode >= 65 && keyCode <= 90) ||
-       (keyCode >= 95 && keyCode <= 122) ||
-       specialKeys.indexOf(keyCode) != -1;
-     document.getElementById("firstnameError").style.display = ret ? "none" : "inline";
-     return ret;
-   }
 
-var specialKeys = new Array();
-specialKeys.push(8); 
-function isAlphaLastName(e){
-     document.getElementById("lastName").innerHTML = "";
-     var keyCode = e.which ? e.which : e.keyCode;
-     var ret =
-       (keyCode >= 65 && keyCode <= 90) ||
-       (keyCode >= 95 && keyCode <= 122) ||
-       specialKeys.indexOf(keyCode) != -1;
-     document.getElementById("lastnameError").style.display = ret ? "none" : "inline";
-     return ret;
-   }
+
+
 
